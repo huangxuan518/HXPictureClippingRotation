58c878817ce640238523334000e4d638bb92658c
huangxuan <770493410@qq.com>
2016-06-20 17:20:37 +0800

    Initial Commit

---
+++ HXPictureClippingRotation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXPictureClippingRotation
+//
+//  Created by Love on 16/6/20.
+//  Copyright © 2016年 黄轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HXPictureClippingRotation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HXPictureClippingRotation
+//
+//  Created by Love on 16/6/20.
+//  Copyright © 2016年 黄轩. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
